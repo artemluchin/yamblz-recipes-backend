f95c0cf297515dfd02f65e752b09da6a0bfaaee2
Artem Luchin <artem.luchin@gmail.com>
2016-08-26 12:32:15 +0300

    change server logic

--- server.js
+++ server.js
@@ -1,52 +1,92 @@
+'use strict'
+
 const express = require('express');
-const fs = require('fs');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
+
 const app = express();
+const MongoClient = mongo.MongoClient;
+const url = 'mongodb://admin:kitchen2016@ds063715.mlab.com:63715/kitchen';
 
-const DB = 'database/database.json';
+const Schema = mongoose.Schema;
 
-app.use(express.static(path.join(__dirname, 'database')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 3000));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+mongoose.connect(url);
+
+const recipesSchema = ({
+  id: Number,
+  title: String,
+  image: String,
+  cookingTime: Number,
+  kcal: Number,
+  complexity: String,
+  category: Number,
+  stages: [{
+    title: String,
+    image: String,
+    steps: [String]
+  }]
+});
+
+const categoriesSchema = ({
+  id: Number,
+  title: String,
+  recipes: [{
+    type: Schema.Types.ObjectId,
+    ref: 'recipes'
+  }]
+})
+
+const Recipe = mongoose.model('recipes', recipesSchema);
+const Category = mongoose.model('categories', categoriesSchema);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to DB');
+});
 
 app.get('/recipes/:id', (req, res) => {
   const recipeId = req.params.id;
 
-  fs.readFile(DB, 'utf-8', (err, data) => {
+  Recipe.find({id: recipeId}).exec((err, recipe) => {
     if (err) throw err;
-
-    const recipes = JSON.parse(data);
-    const recipe = recipes.filter(recipe => recipe.id === Number(recipeId))[0];
-
     res.json(recipe);
-  });
-});
-
-app.get('/categories', (req, res) => {
-  const query = req.query.categories;
-  // console.log(query.split(','))
-
-  fs.readFile(DB, 'utf-8', (err, data) => {
-    if (err) throw err;
-
-    const recipes = JSON.parse(data);
-    const result = {};
-    // query.forEach((id, i) => {
-    //   result.concat(recipes.filter((recipe) => recipe.event));
-    // })
-    recipes.forEach((item) => {
-      if (query.includes(item.event)) {
-        result[item.event] = result[item.event] || []
-        result[item.event].push(item);
-      }
-    })
-
-    res.json(result);
-  });
+  })
 })
 
-app.listen(app.get('port'), () => {
-  console.log("App is listening on port ", app.get('port'));
+app.get('/categories', (req, res) => {
+  const query = req.query.ids;
+  if (!query) {
+    Category
+      .find()
+      .populate('recipes')
+      .exec((err, categories) => {
+        if (err) throw err;
+        res.json(categories)
+      })
+  }
+  else {
+    const categoriesIds = query.split(',');
+
+    Category
+      .find({id: { $in: categoriesIds}})
+      .populate('recipes')
+      .exec((err, category) => {
+        if (err) throw err;
+        res.json(category);
+      });
+  }
 });
+
+app.listen(app.get('port'), () => {
+  console.log('App is listening on port', app.get('port'));
+})
